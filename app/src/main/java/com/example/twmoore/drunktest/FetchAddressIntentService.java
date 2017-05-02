02895e34fd6a722b7be5bad7c55cb75bf85338d0
bpho <bpho@bpho.local>
2017-05-02 01:27:42 -0400

    added drunk screen

---
+++ app/src/main/java/com/example/twmoore/drunktest/FetchAddressIntentService.java
@@ -0,0 +1,97 @@
+package com.example.twmoore.drunktest;
+import android.app.IntentService;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by bpho on 5/2/17.
+ */
+
+public class FetchAddressIntentService extends IntentService {
+    protected ResultReceiver resultReceiver;
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     */
+    public FetchAddressIntentService() {
+        super("FetchAddressIntentService");
+    }
+
+    public FetchAddressIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        resultReceiver = intent.getParcelableExtra(Constants.RECEIVER);
+        String errorMessage = "";
+
+        // Get the location passed to this service through an extra.
+        Location location = intent.getParcelableExtra(
+                Constants.LOCATION_DATA_EXTRA);
+
+        List<Address> addresses = null;
+
+        try {
+            addresses = geocoder.getFromLocation(
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    // In this sample, get just a single address.
+                    1);
+        } catch (IOException ioException) {
+            // Catch network or other I/O problems.
+            errorMessage = "Service not available";
+            Log.e("ERROR ", errorMessage, ioException);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            // Catch invalid latitude or longitude values.
+            errorMessage = "Invalid Lat or Long values";
+            Log.e("ERROR ", errorMessage + ". " +
+                    "Latitude = " + location.getLatitude() +
+                    ", Longitude = " +
+                    location.getLongitude(), illegalArgumentException);
+        }
+
+        // Handle case where no address was found.
+        if (addresses == null || addresses.size()  == 0) {
+            if (errorMessage.isEmpty()) {
+                errorMessage = "No address found";
+                Log.e("ERROR ", errorMessage);
+            }
+            deliverResultToReceiver(Constants.FAILURE_RESULT, errorMessage);
+        } else {
+            Address address = addresses.get(0);
+            ArrayList<String> addressFragments = new ArrayList<String>();
+
+            // Fetch the address lines using getAddressLine,
+            // join them, and send them to the thread.
+            for(int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+                addressFragments.add(address.getAddressLine(i));
+            }
+            Log.i("UPDATE ", "Address has been found.");
+            deliverResultToReceiver(Constants.SUCCESS_RESULT,
+                    TextUtils.join(System.getProperty("line.separator"),
+                            addressFragments));
+        }
+    }
+
+    private void deliverResultToReceiver(int resultCode, String message) {
+        Bundle bundle = new Bundle();
+        bundle.putString(Constants.RESULT_DATA_KEY, message);
+        resultReceiver.send(resultCode, bundle);
+    }
+
+}
+
